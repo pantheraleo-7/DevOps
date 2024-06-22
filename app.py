897546c279b12be9390eff6c6f9cfc6364843060
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-06-23 00:19:36 +0530

    added new feature

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, RedirectResponse
+
 app = FastAPI()
 
 @app.get('/')
 def index():
     return {'hello': 'world'}
 
-@app.get('/alert', response_class=HTMLResponse)
-def read_root():
+@app.get('/js', response_class=HTMLResponse)
+def popup():
     return '<script>alert("This is JavaScript")</script>'
 
 @app.get('/amazon', response_class=RedirectResponse)
